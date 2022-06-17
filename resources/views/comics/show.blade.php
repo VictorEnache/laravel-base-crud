828a6c470813c5779789961bddfdfe2f51ea11cf
VictorEnache <victor22enache@gmail.com>
2022-06-17 09:19:50 +0200

    corretto link nella view show

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -3,5 +3,5 @@
 @section('content')
     <h1>{{$comic->title}}</h1>
     <p>{{$comic->description}}</p>
-    <img src="https://picsum.photos/200/300" alt="">
+    <img src="{{$comic->thumb}}" alt="">
 @endsection
